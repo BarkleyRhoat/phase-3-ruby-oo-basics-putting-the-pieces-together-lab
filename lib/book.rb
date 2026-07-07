0340593f70fd739c51b61b12efaaccb29a56b212
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-07-07 15:55:15 -0400

    putting it all together

--- lib/book.rb
+++ lib/book.rb
@@ -1,1 +1,11 @@
+class Book
+  attr_accessor :genre, :author, :page_count, :title
 
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
